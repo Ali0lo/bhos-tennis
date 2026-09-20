cf10a3cb6b098e0fe8d1c2406ad03950a0961fb8
Ali0lo <aliaze975@gmail.com>
2026-09-20 16:24:31 +0400

    feat(components): implement pulsing radar ping ring animation with animate-ping (#35/100)

--- components/HeroHotspotPin.tsx
+++ components/HeroHotspotPin.tsx
@@ -16,3 +16,14 @@ export interface HeroHotspotPinProps {
   ctaLink?: string;
   avatars?: string[];
 }
+
+export default function HeroHotspotPin(props: HeroHotspotPinProps) {
+  return (
+    <div style={{ top: props.top, left: props.left }} className="absolute z-20">
+      <span className="relative flex h-5 w-5">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-cyan-400 opacity-75" />
+        <span className="relative inline-flex rounded-full h-5 w-5 bg-cyan-500" />
+      </span>
+    </div>
+  );
+}
