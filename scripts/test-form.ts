7ae2ddd7d356494662978c62f682a924c0697bee
Ali0lo <aliaze975@gmail.com>
2026-09-20 16:24:31 +0400

    test(components): add unit test for match form parsing and color assignment (#28/100)

---
+++ scripts/test-form.ts
@@ -0,0 +1,6 @@
+import { MatchFormItem } from '../components/FormDots';
+const sampleForm: MatchFormItem[] = [
+  { id: '1', result: 'W', opponentName: 'Ali Abdulov', score: '3-1', eloDelta: 16 },
+  { id: '2', result: 'L', opponentName: 'Fateh Memmedli', score: '2-3', eloDelta: -14 },
+];
+console.log('Sample form loaded:', sampleForm.length);
