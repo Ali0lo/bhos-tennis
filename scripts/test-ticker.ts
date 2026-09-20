1ba30be39bc63ff692270f3e7841255f14f5f7f2
Ali0lo <aliaze975@gmail.com>
2026-09-20 16:24:30 +0400

    test(components): create test script for motion ticker calculations (#7/100)

---
+++ scripts/test-ticker.ts
@@ -0,0 +1,6 @@
+// Ticker precision test
+function formatNumber(val: number, decimals: number, prefix = '', suffix = '') {
+  return `${prefix}${val.toFixed(decimals)}${suffix}`;
+}
+console.log('Testing Ticker:', formatNumber(1420, 0, '', ' ELO'));
+console.log('Testing Winrate:', formatNumber(78.5, 1, '', '%'));
